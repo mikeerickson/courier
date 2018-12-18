380ee07bcd8f5584a8175af1e22fa9993333d563
Mike Erickson <codedungeon@gmail.com>
2018-12-17 21:14:11 -0800

    initial commit

---
+++ src/Runner.js
@@ -0,0 +1,26 @@
+const { Confirm } = require("enquirer");
+const colors = require("ansi-colors");
+const execa = require("execa");
+
+class Runner {
+  constructor(command, args) {
+    if (args.hasOwnProperty("force") && !args.force) {
+      const prompt = new Confirm({
+        name: "pkg",
+        message: "Are you sure you wish to use npm?"
+      });
+
+      prompt
+        .run()
+        .then(answer => this.executeCommand(process.argv))
+        .catch(console.error);
+    } else {
+      this.executeCommand(process.argv);
+    }
+  }
+  executeCommand(argv) {
+    console.log(argv);
+  }
+}
+
+module.exports = Runner;
